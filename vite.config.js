a57adf28597cf5ae6f4fd3352383f77fab9b1a5b
cll <ndcaolonglong@sina.com>
2023-08-05 15:50:47 +0800

    完成了主题色的配置

--- vite.config.js
+++ vite.config.js
@@ -16,13 +16,24 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        // 1.配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: 'sass' })
+      ]
     })
   ],
   resolve: {
     alias: {
       // 实际的路径转换 @ -> src
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
     }
   }
 })
